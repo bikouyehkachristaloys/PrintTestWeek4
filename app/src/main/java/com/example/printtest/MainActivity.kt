e8a941d08d5935b75f7c4c60eff6ef75120e9ec0
BIKOU <bikouyehkachristaloys@gmail.com>
2020-08-16 03:31:55 -0700

    Phoenix 7

--- app/src/main/java/com/example/printtest/MainActivity.kt
+++ app/src/main/java/com/example/printtest/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.printtest
 
-val n: Int = 3
+fun main(){
+    class Programmer(private val name: String, private val yearOfExperience: Int){
+        fun getName(): String {
+            return name
+        }
 
-private fun main() {
-    println("TimeTen  ${n*10}")
+        fun getExperience(): Int {
+            return yearOfExperience
+        }
+
+    }
+
+    val programmer = Programmer("Phoenix", 15)
+    println(programmer.getName())
+    println(programmer.getExperience())
 }
-
